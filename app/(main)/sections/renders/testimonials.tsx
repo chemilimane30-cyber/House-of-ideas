31a9b26f29b2c74221472195c9e6c93c814456c8
chemil imane <262844823+chemilimane30-cyber@users.noreply.github.com>
2026-08-16 22:46:51 +0000

    delete image

--- app/(main)/sections/renders/testimonials.tsx
+++ app/(main)/sections/renders/testimonials.tsx
@@ -2,7 +2,7 @@
 
 
 
-import img5 from '@/assets/diplomas images/image (6).jpeg'
+
 import img6 from '@/assets/diplomas images/image (7).png'
 import img7 from '@/assets/diplomas images/image (8).jpg'
 
@@ -15,7 +15,7 @@ import React from "react";
 
 import {ReviewForm} from "@/components/fragmenets/forms/review-form";
 
-const images :imgtype[] = [{id:1 ,img:img4}, {id:3,img:img1} ,{id:5,img:img6},{id:6,img:img7}]
+const images :imgtype[] = [{id:1 ,img:img4}, {id:3,img:img1} ,{id:6,img:img7}]
 
 
 
